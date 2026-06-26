1c6808381a614e1448d2c2d8a9b78ec43e1566c3
Md Abdul Basit <basitmdabdul57@gmail.com>
2026-06-26 14:29:51 +0530

    Time: 3 ms (46.15%), Space: 10 MB (11.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.length();
+        if(s.length()!=t.length())return false;
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+        for(int i = 0; i<n; i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
+        }
+        for(auto it:mp1){
+            // if value in mmp1 is not equals to value in mp2
+            // then return false
+            if(mp1!=mp2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
