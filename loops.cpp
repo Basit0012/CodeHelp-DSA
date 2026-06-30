d93d342a82cd171a222da6063eda6f471e69da48
Basit0012 <basitmdabdul57@gmail.com>
2026-06-30 15:37:21 +0530

    Add loop and staircase pattern programs

---
+++ loops.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    while (n != 2)
+    {
+        cout<<"Enter Number: ";
+        cin>>n;
+        
+    }
+    cout<<"Your Output is 2 as expected nigga";
+    return 0;
+}
---
+++ staircase.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int count = 2;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < count; j++) {
+            cout << "*";
+        }
+        cout << endl;
+        if (i % 2 == 0) {
+            count ++;
+        }
+    }
+
+    return 0;
+}
