7c91a49952c71ebb5b56c88dc13ec678e23f023e
Basit0012 <basitmdabdul57@gmail.com>
2026-07-01 11:18:50 +0530

    Add basic function examples

---
+++ function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int add(3,4);
+    adds(4,5);
+    return 0;
+}
+
+int add(int a, int b){
+    return a+b;
+}
+
+void adds(int a,int b){
+    cout<<a+b<<endl;
+}
