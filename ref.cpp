4bfc1a20523b9fb207cdcfe6482073b4e6a676b4
Basit0012 <basitmdabdul57@gmail.com>
2026-07-01 15:15:15 +0530

    Add ref

---
+++ ref.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=345;
+    int *p = &a;
+    cout<<p<<endl;
+    cout<<*p;
+    //  create a swap function using pointers
+    return 0;
+}
