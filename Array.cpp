d00d165bdf1d9cc540788b18bfc8a1521273916c
Basit0012 <basitmdabdul57@gmail.com>
2026-07-02 12:05:58 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -11,8 +11,24 @@
 using namespace std;
 int main(){
     // Arrays
-    int arr[8]={1,2,3,4,5,6,7,8};
+    // uses stack memory
+    // int arr[8]={1,2,3,4,5,6,7,8};
     // Array using new operator
-    int *arr1=new int[5];
-    
+    // uses heap memory
+    // int *arr1=new int[5];
+    // Write a Program to take input from user and print the array and its sum
+    int arr[4];
+    cout<<"Enter Numbers: ";
+    for(int i = 0; i< 4; i++){
+        cin>>arr[i];
+    }
+    cout<< "Array Elements: ";
+    for(int i = 0; i < 4; i++){
+        cout<<arr[i] << " ";
+    }
+    int sum = 0;
+    for(int i=0;i<4;i++){
+        sum = sum + arr[i];
+    }
+    cout<<"\nSum = "<<sum;
 }
