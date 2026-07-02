49d185a9420b7eef552d6268ded0372e59a80fc5
Basit0012 <basitmdabdul57@gmail.com>
2026-07-02 11:05:34 +0530

    Add Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,18 @@
+// Arrays stores similar data
+// Stores in contigous memory allocation => Homogenous Data
+// Random Indexing  => Each element has an index
+// Fixed in SIZE => Static in Nature +>Static Array
+// It is a LINEAR DATA STRUCTURE
+// Elements are arranged one after another 
+// Contigous Memory
+// Array is a collection of similar data types
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // Arrays
+    int arr[8]={1,2,3,4,5,6,7,8};
+    // Array using new operator
+    int *arr1=new int[5];
+    
+}
