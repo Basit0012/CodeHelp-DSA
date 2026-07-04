977cde257211357598be6f6ff4ebe839ebdb9949
Md Abdul Basit <basitmdabdul57@gmail.com>
2026-07-04 12:21:06 +0530

    Time: 3 ms (45.01%), Space: 8.6 MB (65.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int original = x;
+        long long rev = 0;
+        int rem = 0;
+        
+
+      while(x > 0){
+        rem = x % 10;//Last Digit
+        rev = (10 * rev) + rem;//into 10 
+        x = x / 10;
+      }  
+      if(rev == original) return true;
+      else return false;
+    }
+};
