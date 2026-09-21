471ce1a9a0f7a6563069e545c7d3aca6dc2656bd
Basit0012 <basitmdabdul57@gmail.com>
2026-09-21 16:31:43 +0530

    Add vector example

--- initial.cpp
+++ initial.cpp
@@ -18,3 +18,22 @@ int main(){
 
     // cout<<sizeof/sizeof(brr[0]);
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    int index = 2;
+
+    for (int i = index; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+}
